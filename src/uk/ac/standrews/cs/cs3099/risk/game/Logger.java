f5790dc8980739140f911d8f797a32e02a34a7f5
zigrab <zigrab@users.noreply.github.com>
2014-10-28 07:53:24 +0000

    Skeleton

---
+++ src/uk/ac/standrews/cs/cs3099/risk/game/Logger.java
@@ -0,0 +1,42 @@
+package uk.ac.standrews.cs.cs3099.risk.game;
+
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+/**
+ * Logger Class
+ * Adds useful information to standard output
+ */
+public class Logger {
+
+    private static DateFormat df;
+
+    /**
+     * Static initialiser
+     * Just sets up the DateFormat object
+     */
+    static
+    {
+        df = new SimpleDateFormat("HH:mm:ss");
+    }
+
+    /**
+     * Print method
+     * Prints a normal string to standard output
+     */
+    public static void print(String s)
+    {
+        System.out.println(String.format("%s - %s", df.format(new Date()), s));
+    }
+
+    /**
+     * Formatted print
+     * Another wrapper around System.out.printf
+     */
+    public static void printf(String fmt, Object ... args)
+    {
+        System.out.println(String.format("[%s] -- %s", df.format(new Date()), String.format(fmt, args)));
+    }
+
+}
---
+++ src/uk/ac/standrews/cs/cs3099/risk/game/Player.java
@@ -0,0 +1,71 @@
+package uk.ac.standrews.cs.cs3099.risk.game;
+
+import java.net.*;
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * Player Class
+ * Abstractions of what every player should have (including local)
+ */
+public class Player {
+
+    // Unique per-player values
+    private int id;
+    private String name;
+
+    // Game state properties
+    private List<Card> usedcards;
+    private List<Card> hiddencards;
+
+    private int totalarmies;
+    private List<Territory> ownedterritories;
+
+    // Network properties
+    private InetAddress address;
+    private short port;
+    private Socket connection;
+
+    //private PublicKey pubkey;
+    //private PrivateKey privkey;
+
+
+    public Player(int id, InetAddress address, short port, String name)
+    {
+        this.id = id;
+        this.name = name;
+
+        usedcards = new ArrayList<Card>();
+        hiddencards = new ArrayList<Card>();
+
+        totalarmies = 0;
+        ownedterritories = new ArrayList<Territory>();
+
+        this.address = address;
+        this.port = port;
+        // Set up connection to other player here?
+    }
+
+    public boolean canPlaceArmies(Territory t)
+    {
+        return ownedterritories.contains(t);
+    }
+
+    public boolean canMoveArmies(Territory src, Territory dst, int amount)
+    {
+        return src.getArmies() > amount - 1 && src.isLinkedTo(dst) &&
+               ownedterritories.contains(src) && ownedterritories.contains(dst);
+    }
+
+    public void addCard(Card c)
+    {
+        hiddencards.add(c);
+    }
+
+    public void playCard(Card c)
+    {
+        hiddencards.remove(c);
+        usedcards.add(c);
+    }
+
+}
