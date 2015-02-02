0ea4d8379c1ae0b63514a42f76884e06b1c10bbe
zigrab <zigrab@users.noreply.github.com>
2015-02-02 16:39:23 +0000

    Added some name generation during computational complexity

--- src/uk/ac/standrews/cs/cs3099/risk/game/Main.java
+++ src/uk/ac/standrews/cs/cs3099/risk/game/Main.java
@@ -1,5 +1,7 @@
 package uk.ac.standrews.cs.cs3099.risk.game;
 
+import java.util.Random;
+
 /**
  * Main Class
  * Deals with orchestration of the entire game
@@ -8,7 +10,14 @@ public class Main {
 
     public static void main(String[] args)
     {
-        Logger.print("Risk - Initial Commit");
+        Logger.print("Risk - Have some countries:");
+
+        Map m = new Map();
+        Random r = new Random();
+
+        for (int i = 0; i < 25; i++)
+            Logger.print(m.getRandomName(r));
+
         System.exit(0);
     }
 
--- src/uk/ac/standrews/cs/cs3099/risk/game/Map.java
+++ src/uk/ac/standrews/cs/cs3099/risk/game/Map.java
@@ -2,6 +2,7 @@ package uk.ac.standrews.cs.cs3099.risk.game;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Map Class
@@ -12,6 +13,38 @@ public class Map {
     private List<Continent> continents;
     private List<Territory> territories;
 
+    private String[][] territoryNames = {{"Act", "Afr", "Ag", "Agr", "Alb", "Am", "An", "Angl", "Ant", "Aqu", "Ar", "Arc", "As", "Atl", "Austr",
+                                          "Eb", "Eg", "Eng", "Er", "Esc", "Esp", "Est", "Eth", "Etr", "Eul", "Euph", "Eur", "Ib", "Ind", "It",
+                                          "Ug", "Urs", "Ut", "Ur", "Ol", "Olv", "Olm", "Or", "Orm", "Ord", "Os"},
+                                         {"Bol", "Br", "Brund", "Burg", "Byth", "Byz", "Call", "Cas", "Casp", "Cast", "Cast", "Castr", "Cath",
+                                          "Cel", "Ces", "Ch", "Chin", "Cor", "Cr", "Cypr", "Dal", "Dam", "Dan", "Ferr", "Flor", "Gall", "Gen",
+                                          "Germ", "Gran", "Helv", "Hib", "Jap", "Jud", "Laur", "Lib", "Lith", "Luc", "Lyr", "Maj", "Mal", "Mall",
+                                          "Man", "Manch", "Mars", "Merc", "Min", "Mir", "Mol", "Myr", "Myt", "Nam", "Nar", "Nor", "Nub", "Pan",
+                                          "Par", "Pel", "Per", "Pont", "Rhod", "Rhyd", "Rom", "Russ", "Sal", "Sar", "Scot", "Seg", "Senz", "Sib",
+                                          "Spar", "Sum", "Sw", "Sylv", "Tasm", "Terr", "Tes", "Th", "Theon", "Tim", "Tir", "Tr", "Tun", "Tyr",
+                                          "Vir", "Xan", "Zan", "Zeel", "Zen", "Zim"},
+                                         {"ab", "ac", "ad", "ag", "al", "an", "and", "ann", "ant", "anth", "ar", "arc", "arct", "ard", "arn",
+                                          "art", "as", "at", "atr", "av", "en", "enn", "ent", "er", "ern", "es", "et", "ev", "ian", "iat", "ib",
+                                          "ic", "iet", "il", "in", "inth", "is", "it", "itr", "iv", "on", "ont", "op", "or", "orc", "os", "ov",
+                                          "uan", "uar", "un", "und", "ur", "yc", "yg", "ymn", "yn", "yr", "ys"},
+                                         {"a", "aea", "aia", "ana", "and", "as", "ea", "ene", "eos", "ia", "ias", "ica", "ini", "ion", "is",
+                                          "ium", "ius", "on", "ona", "or", "ova", "ul", "um", "ur", "us", "ya", "ano", "ar", "ir"}
+                                        };
+
+    /**
+     * Random territory name
+     * Generates a random territory name, since territories have no stored value currently in the spec
+     */
+    public  String getRandomName(Random r)
+    {
+        int n = r.nextInt(3);
+        String end = territoryNames[3][r.nextInt(territoryNames[3].length - 1)];
+
+        if (n < 2)
+            return territoryNames[n][r.nextInt(territoryNames[n].length - 1)] + territoryNames[2][r.nextInt(territoryNames[2].length - 1)] + end;
+
+        return territoryNames[0][r.nextInt(territoryNames[0].length - 1)] + end;
+    }
 
     public Map()
     {
