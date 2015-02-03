a8d55ff11aac9658b12efbe5030dfc2a918f983d
zigrab <zigrab@users.noreply.github.com>
2015-02-03 15:04:22 +0000

    Some regex magic, now run.sh runs all tests (whoops)

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-java -cp bin:lib/* org.junit.runner.JUnitCore uk.ac.standrews.cs.cs3099.risk.tests.MainTest
+function listtests() {
+    ls fullsrc/tests/* | sed 's/fullsrc\/tests\//uk\.ac\.standrews\.cs\.cs3099\.risk\.tests\./g' | sed 's/Test.*\.java/Test/g'
+}
+
+java -cp bin:lib/* org.junit.runner.JUnitCore `listtests`
 
 echo "------------------------------------------------------------------------------"
 
