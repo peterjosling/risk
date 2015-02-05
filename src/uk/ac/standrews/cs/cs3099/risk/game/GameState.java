4dd9008e8601b1b58809e02b944a5263adb4dc38
Peter Josling <prj2@st-andrews.ac.uk>
2015-02-05 14:49:25 +0000

    Add GameState methods to get territories for a specific owner

--- src/uk/ac/standrews/cs/cs3099/risk/game/GameState.java
+++ src/uk/ac/standrews/cs/cs3099/risk/game/GameState.java
@@ -1,5 +1,7 @@
 package uk.ac.standrews.cs.cs3099.risk.game;
 
+import java.util.ArrayList;
+
 /**
  * GameState Class
  * Stores the entire game and controls board movement
@@ -41,5 +43,31 @@ public class GameState {
 	{
 		removeArmiesForTerritory(from, armies);
 		addArmiesForTerritory(to, armies);
+	}
+
+	/**
+	 * @return Array of {@link Territory} instances which have no owner.
+	 */
+	public Territory[] getUnclaimedTerritories()
+	{
+		return getTerritoriesForPlayer(null);
+	}
+
+	/**
+	 * Get all territories currently owned by a player.
+	 * @param player The player to find territories for.
+	 * @return Array of {@link Territory}.
+	 */
+	public Territory[] getTerritoriesForPlayer(Player player)
+	{
+		ArrayList<Territory> territories = new ArrayList<Territory>();
+
+		for (Territory territory : map.getTerritories()) {
+			if (territory.getOwner() == player) {
+				territories.add(territory);
+			}
+		}
+
+		return territories.toArray(new Territory[0]);
 	}
 }
