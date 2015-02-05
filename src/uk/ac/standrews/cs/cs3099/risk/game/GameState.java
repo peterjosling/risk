012d19517431001e5612e7de6332105b61ac1800
Peter Josling <prj2@st-andrews.ac.uk>
2015-02-05 12:41:11 +0000

    Remove players list from GameState class
    Players are held in Game instance

--- src/uk/ac/standrews/cs/cs3099/risk/game/GameState.java
+++ src/uk/ac/standrews/cs/cs3099/risk/game/GameState.java
@@ -1,14 +1,11 @@
 package uk.ac.standrews.cs.cs3099.risk.game;
 
-import java.util.HashSet;
-
 /**
  * GameState Class
  * Stores the entire game and controls board movement
  */
 public class GameState {
 
-	private HashSet<Player> players;
 	private Map map;
 	private Object deck;
 
