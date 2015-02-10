ac8ba9bd31c55059c10c0e7c63d80e516a7c7200
njk2 <njk2@st-andrews.ac.uk>
2015-02-10 17:58:39 +0000

    implemented getMove for Fortify, Attack and Assign Army.

--- src/uk/ac/standrews/cs/cs3099/risk/game/LocalPlayer.java
+++ src/uk/ac/standrews/cs/cs3099/risk/game/LocalPlayer.java
@@ -14,6 +14,36 @@ public class LocalPlayer extends Player {
 	@Override
 	public Move getMove(MoveType type)
 	{
+		Move move;
+		int id = this.getId();
+		int ackId = 1;
+		int sourceTerritory;
+		int destinationTerritory;
+		int armies;
+		switch (type){
+			case DEPLOY:
+				break;
+			case FORTIFY:
+				sourceTerritory = 1;
+				destinationTerritory = 2;
+				armies = 1;
+				move = new FortifyMove(id, ackId, sourceTerritory, destinationTerritory, armies);
+				break;
+			case ATTACK:
+				sourceTerritory = 1;
+				destinationTerritory = 2;
+				armies = 1;
+				move = new AttackMove(id, ackId, sourceTerritory, destinationTerritory, armies);
+				break;
+			case ASSIGN_ARMY:
+				destinationTerritory = 1;
+				move = new AssignArmyMove(id, ackId, destinationTerritory);
+				break;
+			case DRAW_CARD:
+				break;
+			case TRADE_IN_CARDS:
+				break;
+		}
 		return null;
 	}
 
