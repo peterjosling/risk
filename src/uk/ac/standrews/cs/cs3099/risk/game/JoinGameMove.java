7b2cfc7b6911f117ce1b9322f819a6dd409f837f
Adam Grant <ag215@st-andrews.ac.uk>
2015-02-17 17:16:54 +0000

    Added JoinGameMove class

---
+++ src/uk/ac/standrews/cs/cs3099/risk/game/JoinGameMove.java
@@ -0,0 +1,31 @@
+package uk.ac.standrews.cs.cs3099.risk.game;
+
+public class JoinGameMove extends Move {
+	private float[] supported_versions;
+	private String[] supported_features;
+	
+	public JoinGameMove(int playerId, int ackId) {
+		super(playerId, ackId);
+	}
+	
+	public JoinGameMove(int playerId, int ackId, float[] supported_versions,
+			String[] supported_features) {
+		super(playerId, ackId);
+		this.supported_versions = supported_versions;
+		this.supported_features = supported_features;
+	}
+
+	public float[] getSupported_versions() {
+		return supported_versions;
+	}
+
+	public String[] getSupported_features() {
+		return supported_features;
+	}
+
+	@Override
+	public MoveType getType() {
+		return null;
+	}
+
+}
