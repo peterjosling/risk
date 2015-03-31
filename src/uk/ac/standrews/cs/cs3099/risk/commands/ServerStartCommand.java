b981ac23156e19669c8c70f3a390a7e96358b0a0
Peter Josling <prj2@st-andrews.ac.uk>
2015-03-31 16:25:09 +0100

    Add ServerStart/ServerConnect commands

---
+++ src/uk/ac/standrews/cs/cs3099/risk/commands/ServerStartCommand.java
@@ -0,0 +1,28 @@
+package uk.ac.standrews.cs.cs3099.risk.commands;
+
+/**
+ * Move to represent the server_start command. Used only for the internal frontend server.
+ */
+public class ServerStartCommand extends Command {
+	private ServerStartPayload payload;
+
+	public ServerStartCommand(int playerId)
+	{
+		super(playerId);
+	}
+
+	public int getPort()
+	{
+		return payload.port;
+	}
+
+	@Override
+	public CommandType getType()
+	{
+		return CommandType.SERVER_START;
+	}
+
+	private class ServerStartPayload {
+		int port;
+	}
+}
