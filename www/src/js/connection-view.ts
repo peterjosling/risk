b8c9b976bfed28e3d0bbef633842029250c1df1d
Peter Josling <prj2@st-andrews.ac.uk>
2015-03-31 16:21:08 +0100

    Fix ConnectionView button event handler

--- www/src/js/connection-view.ts
+++ www/src/js/connection-view.ts
@@ -10,19 +10,22 @@ class ConnectionView extends View<Game> {
 		this.events = <any>{
 			'click .connect-button': 'connectButtonClick'
 		};
+
+		this.delegateEvents();
 	}
 
-	connectButtonClick(e : Event) : void {
+	connectButtonClick(e : Event) : boolean {
 		var hostname : string = this.$('#connection-host').val(),
 			port : number = this.$('#connection-port').val();
 
 		this.model.connect(hostname, port).then(() => {
-
+			this.trigger('gameConnected')
 		}, () => {
 			alert('Failed to connect');
 		});
 
 		// TODO show loading indicator.
+		return false;
 	}
 }
 
