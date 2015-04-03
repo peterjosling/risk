d7315e90a67eb5199c1a07da076e03268eccf508
zigrab <zigrab@users.noreply.github.com>
2015-04-03 12:04:28 +0100

    Added random number generation - as per spec

--- src/uk/ac/standrews/cs/cs3099/risk/game/Main.java
+++ src/uk/ac/standrews/cs/cs3099/risk/game/Main.java
@@ -15,7 +15,7 @@ public class Main {
 	public static void main(String[] args)
 	{
 		String json = "";
-		
+
 	    BufferedReader br;
 		try {
 			br = new BufferedReader(new FileReader("default-map.json"));
@@ -27,7 +27,7 @@ public class Main {
 	            line = br.readLine();
 	        }
 	        json = sb.toString();
-	        
+
 	        br.close();
 		} catch (FileNotFoundException e1) {
 			e1.printStackTrace();
@@ -37,13 +37,39 @@ public class Main {
 		json = json.replaceAll("\t", "");
 
 		System.out.println(json);
-		
-		
+
+		Random r = new Random();
+
+		for (int i = 0; i < 25; i++)
+			Logger.print(m.getRandomName(r));
+
+		Logger.print("------------------------------------------------------------------------------");
+		Logger.print("Risk - And some player names:");
+
+		for (int i = 0; i < 25; i++)
+			Logger.print(p.genName());
+
+		Logger.print("------------------------------------------------------------------------------");
+		Logger.print("Risk - Some random bytes, generated with seed='Key' - RandomNumbers(\"4B6579\"):");
+
+		RandomNumbers rn = new RandomNumbers("4B6579"); // Seeding with 'Key'
+		for (int i = 0; i < 10; i++)
+			System.out.printf("%02X", rn.getRandomByte() & 0xFFL);
+		System.out.println();
+
+		Logger.print("------------------------------------------------------------------------------");
+		Logger.print("Risk - A random int, generated with seed='Key' - RandomNumbers(\"4B6579\"):");
+
+		rn = new RandomNumbers("4B6579");
+		System.out.printf("%08X\n", rn.getRandomInt() & 0xFFFFFFFFL);
+
 		try {
 			LocalGame localGame = new LocalGame(json, 4, 10);
 			localGame.run();
 		} catch (MapParseException e) {
 			e.printStackTrace();
 		}
+
+		System.exit(0);
 	}
 }
