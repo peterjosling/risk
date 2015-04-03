66b1c38854a907f510e7ccb1b8fd0b27400260a6
njk2 <njk2@st-andrews.ac.uk>
2015-04-03 12:07:45 +0100

    Refactored LocalPlayer.getMove into multiple methods.

---
+++ src/uk/ac/standrews/cs/cs3099/risk/game/CommandNotFoundException.java
@@ -0,0 +1,9 @@
+package uk.ac.standrews.cs.cs3099.risk.game;
+
+public class CommandNotFoundException extends Exception {
+
+	public CommandNotFoundException(String message)
+	{
+		super(message);
+	}
+}
