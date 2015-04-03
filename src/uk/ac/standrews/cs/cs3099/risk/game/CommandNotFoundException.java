561058397f8daa88af554caa2e907198f3b7f36c
njk2 <njk2@st-andrews.ac.uk>
2015-04-03 16:53:38 +0100

    Removed command not found exception

--- src/uk/ac/standrews/cs/cs3099/risk/game/CommandNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package uk.ac.standrews.cs.cs3099.risk.game;
-
-public class CommandNotFoundException extends Exception {
-
-	public CommandNotFoundException(String message)
-	{
-		super(message);
-	}
-}
