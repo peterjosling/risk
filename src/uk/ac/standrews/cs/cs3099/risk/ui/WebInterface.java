6e0a78c376f1ccbb9dbffae7118ae4634dac0cb5
Peter Josling <prj2@st-andrews.ac.uk>
2015-04-04 23:17:54 +0100

    Add TODO in web interface

--- src/uk/ac/standrews/cs/cs3099/risk/ui/WebInterface.java
+++ src/uk/ac/standrews/cs/cs3099/risk/ui/WebInterface.java
@@ -8,5 +8,7 @@ public class WebInterface {
     public static void main(String args[]) {
         WebSocketServer server = new WebSocketServer(new InetSocketAddress(PORT));
         server.start();
+
+        // TODO launch a file server to serve the web interface
     }
 }
