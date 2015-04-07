71382baa8a78412dc15c7b94adbb271df2585daf
Peter Josling <prj2@st-andrews.ac.uk>
2015-04-07 18:16:24 +0100

    Render map SVG

--- www/src/js/map-view.ts
+++ www/src/js/map-view.ts
@@ -1,5 +1,8 @@
 import View = require('./view');
+import Game = require('./game');
 
-class MapView extends View<Backbone.Model> {}
+class MapView extends View<Game> {
+	template = <Function>require('../hbs/map-view.hbs')
+}
 
 export = MapView;
