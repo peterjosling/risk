c78c9604223ce3bdf236698c2cd85c6dcb313ca5
Peter Josling <prj2@st-andrews.ac.uk>
2015-04-09 14:34:18 +0100

    Fill territories based on owner colour after map render

--- www/src/js/map-view.ts
+++ www/src/js/map-view.ts
@@ -2,7 +2,26 @@ import View = require('./view');
 import Game = require('./game');
 
 class MapView extends View<Game> {
-	template = <Function>require('../hbs/map-view.hbs')
+	template = <Function>require('../hbs/map-view.hbs');
+
+	render() {
+		super.render();
+		this.updateMapState();
+		return this;
+	}
+
+	updateMapState() {
+		this.model.map.territories.forEach(function(territory) {
+			var owner = territory.getOwner();
+			var colour = '#FFFFFF';
+
+			if (owner) {
+				colour = owner.getColour();
+			}
+
+			this.$('[data-territory-id' + territory.id + ']').attr('fill', colour);
+		}, this);
+	}
 }
 
 export = MapView;
