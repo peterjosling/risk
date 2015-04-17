e9dcf70a5099c2869f51bc62499efd94008745aa
Peter Josling <prj2@st-andrews.ac.uk>
2015-04-17 12:36:09 +0100

    Launch web server and open browser in WebInterface
    Add SimpleWebServer.jar dependency

--- src/uk/ac/standrews/cs/cs3099/risk/ui/WebInterface.java
+++ src/uk/ac/standrews/cs/cs3099/risk/ui/WebInterface.java
@@ -1,14 +1,33 @@
 package uk.ac.standrews.cs.cs3099.risk.ui;
 
+import org.jibble.simplewebserver.SimpleWebServer;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.net.URI;
 
 public class WebInterface {
-    private static final int PORT = 7574;
+	private static final int PORT = 7574;
 
-    public static void main(String args[]) {
-        WebSocketServer server = new WebSocketServer(new InetSocketAddress(PORT));
-        server.start();
+	public static void main(String args[]) {
+		WebSocketServer webSocketServer = new WebSocketServer(new InetSocketAddress(PORT));
+		SimpleWebServer webServer;
+		webSocketServer.start();
 
-        // TODO launch a file server to serve the web interface
-    }
+		try {
+			webServer = new SimpleWebServer(new File("www/"), 9091);
+		} catch (IOException e) {
+			System.out.println("Failed to launch web server on port 9091");
+			System.exit(1);
+		}
+
+		// Launch the default web browser to show the interface.
+		try {
+			Desktop.getDesktop().browse(new URI("http://localhost:9091"));
+		} catch (Exception e) {
+			System.out.println("Failed to launch web browser.");
+		}
+	}
 }
