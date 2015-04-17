e8f5bcb310371c22ad4f88c0a4744669cb0c0f1c
Peter Josling <prj2@st-andrews.ac.uk>
2015-04-17 15:46:38 +0100

    Launch server on server_connect command

--- src/uk/ac/standrews/cs/cs3099/risk/ui/WebSocketServer.java
+++ src/uk/ac/standrews/cs/cs3099/risk/ui/WebSocketServer.java
@@ -95,8 +95,15 @@ public class WebSocketServer extends org.java_websocket.server.WebSocketServer {
 	 */
 	private void startServer(WebSocket ws, ServerStartCommand command)
 	{
-		AbstractGame game = new NetworkedGame(24);
-		Player player = new UIPlayer(ws, 0, "Test player");
+		Player player = new UIPlayer(ws, 0, "Player names not implemented");
+		NetworkedGame game = new NetworkedGame(24);
+		game.setLocalPlayer(player);
 		games.put(ws.getRemoteSocketAddress(), game);
+
+		try {
+			game.startServer(command.getPort());
+		} catch (IOException e) {
+			System.err.println("Couldn't start host server on port " + command.getPort());
+		}
 	}
 }
