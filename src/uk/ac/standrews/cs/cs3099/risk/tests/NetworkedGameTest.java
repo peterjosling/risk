34939e9cf384d555d26ff65d2e1a27e0f2ad4b69
Peter Josling <prj2@st-andrews.ac.uk>
2015-04-17 11:49:34 +0100

    Add beginning of NetworkedGame test

---
+++ src/uk/ac/standrews/cs/cs3099/risk/tests/NetworkedGameTest.java
@@ -0,0 +1,18 @@
+package uk.ac.standrews.cs.cs3099.risk.tests;
+
+import org.junit.Test;
+import uk.ac.standrews.cs.cs3099.risk.network.NetworkedGame;
+
+import java.io.IOException;
+
+public class NetworkedGameTest {
+	@Test
+	public void nonPlayingHostTest() throws IOException
+	{
+		NetworkedGame game = new NetworkedGame(24);
+		game.startServer(1234);
+
+		NetworkedGame client = new NetworkedGame(24);
+		game.connectToServer("localhost", 1234);
+	}
+}
