71f27ee7a3369387a5e8d90f3d5be20ecdb232c0
njk2 <njk2@st-andrews.ac.uk>
2015-04-17 14:15:28 +0100

    Created Acknowledgement class for network

---
+++ src/uk/ac/standrews/cs/cs3099/risk/network/Acknowledgement.java
@@ -0,0 +1,30 @@
+package uk.ac.standrews.cs.cs3099.risk.network;
+
+import java.util.Date;
+
+public class Acknowledgement {
+
+	private Date date;
+	private int ackId;
+	private boolean[] playersAcknowledged;
+
+
+	Acknowledgement(int ackid){
+		date = new Date();
+		this.ackId = ackid;
+		playersAcknowledged = new boolean[6]; //indexed by player ids
+	}
+
+
+	public Date getDate() {
+		return date;
+	}
+
+	public int getAckId() {
+		return ackId;
+	}
+
+	public boolean[] getPlayersAcknowledged() {
+		return playersAcknowledged;
+	}
+}
