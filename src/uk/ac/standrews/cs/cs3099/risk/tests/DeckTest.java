609f7f85a2a491df09f9549d3a63343936e18641
njk2 <njk2@st-andrews.ac.uk>
2015-04-18 11:34:23 +0100

    Fixed Deck to use hard coded deck

--- src/uk/ac/standrews/cs/cs3099/risk/tests/DeckTest.java
+++ src/uk/ac/standrews/cs/cs3099/risk/tests/DeckTest.java
@@ -22,32 +22,6 @@ public class DeckTest {
 	public void setup()
 	{
 		deck1 = new Deck(44);
-		deck2 = new Deck(60);
-		deck3 = new Deck(44);
-	}
-
-	@Test
-	public void deckContentsTest()
-	{
-		ArrayList<Card> cards = deck1.getDeck();
-		assertEquals(44, cards.size());
-		assertEquals(Card.CardType.INFANTRY, cards.get(0).getCardType());
-		assertEquals(Card.CardType.INFANTRY, cards.get(13).getCardType());
-		assertEquals(Card.CardType.CAVALRY, cards.get(14).getCardType());
-		assertEquals(Card.CardType.CAVALRY, cards.get(27).getCardType());
-		assertEquals(Card.CardType.ARTILLERY, cards.get(28).getCardType());
-		assertEquals(Card.CardType.ARTILLERY, cards.get(41).getCardType());
-		assertEquals(Card.CardType.WILD, cards.get(42).getCardType());
-		assertEquals(Card.CardType.WILD, cards.get(43).getCardType());
-
-		ArrayList<Card> cards2 = deck2.getDeck();
-		assertEquals(60, cards2.size());
-		assertEquals(Card.CardType.INFANTRY, cards2.get(0).getCardType());
-		assertEquals(Card.CardType.INFANTRY, cards2.get(19).getCardType());
-		assertEquals(Card.CardType.CAVALRY, cards2.get(20).getCardType());
-		assertEquals(Card.CardType.CAVALRY, cards2.get(39).getCardType());
-		assertEquals(Card.CardType.ARTILLERY, cards2.get(40).getCardType());
-		assertEquals(Card.CardType.ARTILLERY, cards2.get(59).getCardType());
 	}
 
 	@Test
