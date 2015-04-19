c67d9d99c106846d0b381faf1f56ab31f5388ca7
Peter Josling <prj2@st-andrews.ac.uk>
2015-04-19 13:15:38 +0100

    Show toast when starting a turn

--- www/src/js/game-view.ts
+++ www/src/js/game-view.ts
@@ -16,6 +16,8 @@ class GameView extends View<Game> {
 		var playerListView = new PlayerListView({ model: this.model });
 		var mapView = new MapView({model: this.model});
 
+		this.listenTo(this.model, 'change:currentPlayer', this.currentPlayerChange);
+
 		this.childViews = [
 			{
 				view: playerListView,
@@ -26,6 +28,38 @@ class GameView extends View<Game> {
 				el: '#map'
 			}
 		];
+	}
+
+	currentPlayerChange() {
+		var player = this.model.getCurrentPlayer();
+
+		if (player === this.model.self) {
+			this.startTurn()
+		} else {
+			// TODO end turn
+			this.model.showToast(player.name + '\'s turn');
+		}
+	}
+
+	startTurn() {
+		this.model.showToast('Your turn!');
+
+		// Check if we're in the setup phase.
+		var allTerritoriesClaimed = this.model.map.territories.every(territory => {
+			return territory.getOwner() !== null;
+		});
+
+		var toastMessage;
+
+		if (allTerritoriesClaimed) {
+			toastMessage = 'Select a territory to reinforce...';
+		} else {
+			toastMessage = 'Select a territory to claim...';
+		}
+
+		this.model.showToast(toastMessage);
+
+		// TODO highlight territories which can be selected.
 	}
 }
 
