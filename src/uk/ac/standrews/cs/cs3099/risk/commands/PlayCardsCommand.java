4a1004160a6df9db3a577913cc6a3bfb09e77b51
Peter Josling <prj2@st-andrews.ac.uk>
2015-04-21 11:25:47 +0100

    Update PlayCardsCommand to match spec

--- src/uk/ac/standrews/cs/cs3099/risk/commands/PlayCardsCommand.java
+++ src/uk/ac/standrews/cs/cs3099/risk/commands/PlayCardsCommand.java
@@ -4,7 +4,7 @@ import uk.ac.standrews.cs.cs3099.risk.game.Card;
 
 public class PlayCardsCommand extends Command {
 	private String command = "play_cards";
-	private Card[][] payload;
+	private PlayCardsPayload payload = new PlayCardsPayload();
 
 	public PlayCardsCommand(int playerId, int ackId)
 	{
@@ -14,20 +14,35 @@ public class PlayCardsCommand extends Command {
 	public PlayCardsCommand(int playerId, int ackId, Card[][] cards)
 	{
 		this(playerId, ackId);
-		this.payload = cards;
+		this.payload.cards = cards;
+		this.payload.armies = -1;
+
+		if (cards == null) {
+			this.payload = null;
+		}
 	}
 
 	/**
 	 * @return Array of {@link Card} instances. Should be length 3, all unique and all of same type to be valid.
+	 * Returns null if the command has no payload.
 	 */
 	public Card[][] getCards()
 	{
-		return payload;
+		if (payload == null) {
+			return null;
+		}
+
+		return payload.cards;
 	}
 
 	@Override
 	public CommandType getType()
 	{
 		return CommandType.PLAY_CARDS;
+	}
+
+	private class PlayCardsPayload {
+		Card[][] cards;
+		int armies;
 	}
 }
